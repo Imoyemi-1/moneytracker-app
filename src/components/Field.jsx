5a735052e3a84a505a5a17e44dbebe293361fbef
Imoyemi <okeibrahim133@gmail.com>
2025-09-04 21:19:12 -0500

    feat(setup): added selection currencies element
    - Added selection,input and selection container element
    - Styled selection container and input components
    - Added onfocus and onblur logic for input to toggle selection display

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,16 +1,23 @@
-import { RiArrowDropDownFill } from 'react-icons/ri';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import { useState } from 'react';
 
-const Field = ({ label }) => {
+const Field = ({ label, selection, defaultPh, FocusPh }) => {
+  const [isFocused, setIsFocused] = useState(false);
   return (
     // Todo: add accessibility to jsx later
 
-    <div>
-      <label className='font-medium mb-1 text-sm'>{label}</label>
-      <div>
-        <input type='text' />
-        <div></div>
-        <RiArrowDropDownFill />
-        <div></div>
+    <div className='mb-3.5 flex flex-col'>
+      <label className='font-medium mb-1 text-sm '>{label}</label>
+      <div className='w-full flex border border-gray-300 h-9.5 rounded items-center py-2.5  px-3.5 '>
+        {!isFocused && <div className='text-[0.9375rem]'>{selection}</div>}
+        <input
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          className='py-2 pr-7 full outline-0 min-w-12.5 flex-1 border-0 text-[0.9375rem]'
+          type='text'
+          placeholder={isFocused ? FocusPh : defaultPh}
+        />
+        <IoMdArrowDropdown className='cursor-pointer' />
       </div>
     </div>
   );
--- src/setupPage/Form.jsx
+++ src/setupPage/Form.jsx
@@ -1,10 +1,23 @@
+import { useRef, useState } from 'react';
 import Field from '../components/Field';
 
 const Form = () => {
+  const [baseSelection, setBaseSelection] = useState('House');
+
   return (
     <form>
       <div>
-        <Field label='Base Currency' />
+        <Field
+          label='Base Currency'
+          selection={baseSelection}
+          defaultPh=''
+          FocusPh={baseSelection}
+        />
+        <Field
+          label='Additional Currencies (optional)'
+          defaultPh='Select additional currencies'
+          FocusPh='Select additional currencies'
+        />
       </div>
     </form>
   );
