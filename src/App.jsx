ad6a9b07f4ebcf12c739f34b5aeace780c0ad8a4
Imoyemi <okeibrahim133@gmail.com>
2025-09-13 08:57:45 -0500

    feat: add pages to app page for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import SetupPage from './pages/SetupPage';
 import { DropdownProvider } from './contexts/Setup';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import DashboardPage from './pages/DashboardPage';
+import AccountsPage from './pages/AccountsPage';
+import BudgetPage from './pages/BudgetPage';
+import ReportsPage from './pages/ReportsPage';
+import SettingsPage from './pages/SettingsPage';
 import RootLayout from './layout/RootLayout';
 import TransactionsPage from './pages/TransactionsPage';
 import { useEffect, useState } from 'react';
@@ -34,6 +38,22 @@ const App = () => {
         {
           path: 'transactions',
           element: <TransactionsPage />,
+        },
+        {
+          path: 'accounts',
+          element: <AccountsPage />,
+        },
+        {
+          path: 'reports',
+          element: <ReportsPage />,
+        },
+        {
+          path: 'budget',
+          element: <BudgetPage />,
+        },
+        {
+          path: 'settings',
+          element: <SettingsPage />,
         },
       ],
     },
