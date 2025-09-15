7860d2357cd0c0f7330a16fcd7fc808e7f522d55
Imoyemi <okeibrahim133@gmail.com>
2025-09-15 12:26:22 -0500

    feat: handle header pathname on nav change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { useAsideBar } from '../contexts/aside';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 
 const Header = () => {
   const { setOpenSidebar } = useAsideBar();
+  const [currentPageName, setCurrentPageName] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const pathToNameMap = {
+      '/': 'Dashboard',
+      '/transactions': 'Transactions',
+      '/accounts': 'Accounts',
+      '/reports': 'Reports',
+      '/budget': 'Budget',
+      '/settings': 'Settings',
+    };
+    setCurrentPageName(pathToNameMap[location.pathname]);
+  }, [location.pathname]);
 
   return (
     <header className='p-4 h-15.5 bg-blue-bg sticky top-0 w-full shadow-header z-50'>
@@ -11,7 +27,7 @@ const Header = () => {
           onClick={() => setOpenSidebar((prev) => !prev)}
           className='text-[1.6875rem] cursor-pointer'
         />
-        <h2 className='pl-3 font-light flex-1 text-2xl '>Dashboard</h2>
+        <h2 className='pl-3 font-light flex-1 text-2xl '>{currentPageName}</h2>
       </div>
     </header>
   );
