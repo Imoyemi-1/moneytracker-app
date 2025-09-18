70a3044ab1c7df1294856dc2a80e4c24542414f0
Imoyemi <okeibrahim133@gmail.com>
2025-09-18 15:20:00 -0500

    Feat(dashboard): add recent transaction section to dashboard

--- src/dashboardPage/TransactionSection.jsx
+++ src/dashboardPage/TransactionSection.jsx
@@ -1,5 +1,9 @@
 const TransactionSection = () => {
-  return <div>TransactionSection</div>;
+  return (
+    <div className='px-3.5 text-sm min-h-12.5 flex items-center'>
+      No transactions found.
+    </div>
+  );
 };
 
 export default TransactionSection;
