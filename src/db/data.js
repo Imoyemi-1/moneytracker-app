67b2ed8014a9f63fecc3ae07c918b63fde3424ff
Imoyemi <okeibrahim133@gmail.com>
2025-09-18 17:04:10 -0500

    Feat: add save transaction,save tags to indexed db

--- src/db/data.js
+++ src/db/data.js
@@ -2,10 +2,13 @@ import Dexie from 'dexie';
 
 const db = new Dexie('MoneyTrackerDB');
 
-db.version(1).stores({
+db.version(2).stores({
   accounts: '++id, type,  name, showOnDashboard ,currencies',
   settings: 'key',
   exchangeRates: 'id',
+  transactions:
+    '++id, type, firstAccountTransaction, secondAccountTransaction, note, date',
+  tags: '++id, name',
 });
 
 export default db;
--- src/hooks/useAccount.js
+++ src/hooks/useAccount.js
@@ -12,4 +12,40 @@ const useSaveAccount = async (account) => {
   return accountId;
 };
 
-export { useSaveAccount };
+const useSaveTransactions = async (transaction) => {
+  // save transaction to db itself
+  const transactionsId = await db.transactions.add({
+    type: transaction.type,
+
+    // for  account and amount that use for debit expense or add income
+    firstAccountTransaction: {
+      id: transaction.firstAccountId,
+      amount: transaction.firstAccountAmount,
+    },
+
+    // for  account and amount that use receive amount from transfer
+    secondAccountTransaction: {
+      id: transaction?.secondAccountId,
+      amount: transaction?.secondAccountAmount,
+    },
+
+    // add note and date for transactions
+    note: transaction.note,
+    date: transaction.date,
+
+    // add tags for transactions
+    tags: transaction.tags,
+  });
+
+  return transactionsId;
+};
+
+const useSaveTags = async (tag) => {
+  // save account to db itself
+  const accountId = await db.tags.add({
+    name: tag,
+  });
+
+  return accountId;
+};
+export { useSaveAccount, useSaveTransactions, useSaveTags };
