da2d5d9d9ac7897c9ae36826ed148e6c97b69aa9
Imoyemi <okeibrahim133@gmail.com>
2025-09-19 20:53:39 -0500

    feat: added transaction details

--- src/dashboardPage/TransactionSection.jsx
+++ src/dashboardPage/TransactionSection.jsx
@@ -1,5 +1,45 @@
+import { useContext } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import { MdOutlineEdit } from 'react-icons/md';
+
 const TransactionSection = () => {
-  return (
+  const { transactions } = useContext(AppContext);
+  console.log(transactions);
+
+  const transactionList = transactions.map((list) => (
+    <div key={list.id}>
+      <div>{list.date}</div>
+      <div>
+        {list.accountTransactionInfo[0]?.name}
+        {list.accountTransactionInfo[1]?.name}
+        {list.tags.map((tag, index) => (
+          <div key={index}>{tag}</div>
+        ))}
+        {list.note && <span>{list.note}</span>}
+      </div>
+      <div>
+        <span>
+          {list.accountTransactionInfo[0]?.amount}{' '}
+          {list.accountTransactionInfo[0]?.code}
+        </span>{' '}
+        {list.accountTransactionInfo[1]?.code !==
+        list.accountTransactionInfo[0]?.code ? (
+          <span>
+            {list.accountTransactionInfo[1]?.amount}{' '}
+            {list.accountTransactionInfo[1]?.code}
+          </span>
+        ) : null}
+      </div>
+      <div>
+        <button>
+          <MdOutlineEdit />
+        </button>
+      </div>
+    </div>
+  ));
+  return transactions.length > 0 ? (
+    transactionList
+  ) : (
     <div className='px-3.5 text-sm min-h-12.5 flex items-center'>
       No transactions found.
     </div>
