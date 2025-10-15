a793677cc452ae89a3b9cb4852a315cc86fe6132
Imoyemi <okeibrahim133@gmail.com>
2025-10-14 19:55:07 -0500

    feat: added work in progress banner to budget page

--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -1,7 +1,15 @@
+import { FaHourglassHalf } from 'react-icons/fa6';
+
 const BudgetPage = () => {
   return (
-    <div className='md:max-w-[calc(100%-7.8rem)] md:ml-[7.8rem] md:mt-3.5'>
-      BudgetPage
+    <div className='md:max-w-[calc(100%-7.8rem)] md:ml-[7.8rem] md:mt-3.5 md:px-1.5'>
+      <div className='flex items-center w-full max-w-[60rem] mx-auto px-4 py-4.5 rounded shadow-[inset_0_0_0_1px_rgba(34,36,38,0.22),0_0_0_0_transparent] bg-[#f8f8f9]'>
+        <FaHourglassHalf className='text-5xl opacity-[0.8] mr-7' />
+        <div className='flex-1 '>
+          <div className='text-lg font-bold '>Work in Progress</div>
+          <p>This page is not ready yet, check back later 😉</p>
+        </div>
+      </div>
     </div>
   );
 };
